e0e02871e67d39bee96ea53a0162aa31f5255e81
BensonGitau <bensonkamaugitau@gmail.com>
2024-07-04 21:01:01 +0300

    complete

--- server/app.py
+++ server/app.py
@@ -52,6 +52,28 @@ api.add_resource(ClearSession, '/clear')
 api.add_resource(IndexArticle, '/articles')
 api.add_resource(ShowArticle, '/articles/<int:id>')
 
+class Login(Resource):
+    def post(self):
+        user = User.query.filter(User.username == request.json['username']).first()
+        session['user_id'] = user.id
+        return user.to_dict(), 200
+api.add_resource(Login, '/login')
+
+class Logout(Resource):
+    def delete(self):
+        session['user_id'] = None
+        return {'message': 'Logged out'}, 204
+api.add_resource(Logout, '/logout')
+
+class CheckSession(Resource):
+    def get(self):
+        user = User.query.filter(User.id == session.get('user_id')).first()
+        if user:
+            return user.to_dict()
+        else:
+            return {}, 401
+api.add_resource(CheckSession, '/check_session')
+
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
